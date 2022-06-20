172a3bf74fdc7931185eccddfeea67aa9e6e65d8
malikkurosaki <kurosakiblackangel@gmail.com>
2022-06-20 16:13:52 +0800

    ya

--- server/bin/index.js
+++ server/bin/index.js
@@ -4,6 +4,29 @@ const path = require("path");
 const _server = path.join(__dirname, "../../server");
 const _client = path.join(__dirname, "../../client");
 const execSync = require("child_process").execSync;
+const fs = require("fs");
+
+
+choices1("")
+
+/** @typedef {"local" | "server" | "mode" | "build"} Chooice1 */
+/** @typedef {"dev_web" | "pro_web" | "dev_mobile"} Chooice2 */
+/** @typedef {"run_client_web" | "run_client_android" | "run_server" | "git_push" | "git_clear"} Chooice3 */
+
+
+/** @param {Chooice1} title */
+function choices1(title) {
+  return {
+    choices: {
+      title: title,
+    value: title,
+    },
+    /** @param {void} onSubmit */
+    isMe(answer, onSubmit) {
+      if (answer === title) onSubmit();
+    },
+  };
+}
 
 prompts(
   {
@@ -11,24 +34,87 @@ prompts(
     name: "pilihan",
     message: "pilih menunya",
     choices: [
-      { title: "local", value: "local" },
-      { title: "server", value: "server" },
+      choices1("local").choices,
+      choices1("server").choices,
+      choices1("build").choices,
+      choices1("mode").choices,
     ],
   },
   {
     onSubmit: (_, answer) => {
-      switch (answer) {
-        case "local":
-          local();
-          break;
-        case "server":
-          break;
-        default:
-          console.log("tidak ada pilihan");
-      }
+      choices1("local").isMe(answer, local);
+      choices1("server").isMe(answer, server);
+      choices1("build").isMe(answer, build);
+      choices1("mode").isMe(answer, mode);
     },
   }
 );
+
+function server() {
+  console.log("server");
+}
+
+
+function mode() {
+  prompts(
+    {
+      type: "select",
+      name: "name",
+      message: "select menu",
+      choices: [
+        { title: "mode development web", value: "dev_web" },
+        { title: "mode production", value: "pro_web" },
+        { title: "mode developmen mobile", value: "dev_mobile" },
+      ],
+    },
+    {
+      onSubmit: (_, answer) => {
+        setMode(answer);
+      },
+    }
+  );
+}
+
+/**
+ *
+ * @param {Chooice2} modenya
+ */
+function setMode(modenya) {
+  let devWeb = "http://localhost:3000";
+  let proWeb = "https://makurostudio.my.id";
+  let devMobile = "http://192.168.43.112:3000";
+  let con = "";
+
+  let connFile = path.join(__dirname, "./../../client/lib/config.dart");
+
+  switch (modenya) {
+    case "dev_web":
+      con = devWeb;
+      break;
+    case "pro_web":
+      con = proWeb;
+      break;
+    case "dev_mobile":
+      con = devMobile;
+      break;
+    default:
+      con = devWeb;
+      break;
+  }
+
+  let target = `
+  class Config{
+    static const String host = "${con}";
+  }`;
+
+  try {
+    fs.writeFileSync(connFile, target, { encoding: "utf-8" });
+
+    console.log("mode set to " + modenya);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function local() {
   prompts(
@@ -41,6 +127,7 @@ function local() {
         { title: "run client android", value: "run_client_android" },
         { title: "run server", value: "run_server" },
         { title: "git push", value: "git_push" },
+        { title: "git clear", value: "git_clear" },
       ],
     },
     {
@@ -58,7 +145,12 @@ function local() {
             execSync(`cd ${_server} && nodemon .`, { stdio: "inherit" });
             break;
           case "git_push":
-            execSync(`git add . && git commit -m ya && git push origin main`, { stdio: "inherit" });
+            execSync(`git add . && git commit -m ya && git push origin main`, {
+              stdio: "inherit",
+            });
+            break;
+          case "git_clear":
+            execSync(`git rm -r --cached .`, { stdio: "inherit" });
             break;
           default:
             console.log("tidak ada pilihan");
@@ -67,3 +159,9 @@ function local() {
     }
   );
 }
+
+/**
+ *
+ * @param value {"web" | "android"}
+ */
+function build(value) {}
