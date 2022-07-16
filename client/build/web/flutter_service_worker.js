a6c02c7fb46f9d220ad7941e899074b6d4a508d5
malikkurosaki <kurosakiblackangel@gmail.com>
2022-07-16 11:30:46 +0800

    x

--- client/build/web/flutter_service_worker.js
+++ client/build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6600b79256acfe9ebcb26e0776346a26",
-"index.html": "5e053ae4536a24c0d399132e6b3dbaca",
-"/": "5e053ae4536a24c0d399132e6b3dbaca",
-"main.dart.js": "5de022d20178a2f25707479a3455aa4f",
+"index.html": "2b5768107fe688347c262a56a48d76d8",
+"/": "2b5768107fe688347c262a56a48d76d8",
+"main.dart.js": "bcd5a1ba544a947354b5e517651e0871",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "faviconbak.png": "5dcef449791fa27946b3d35ad8803796",
 "favicon.png": "884d197c7139780e1e638b6e76d7312c",
