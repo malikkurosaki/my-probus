8b9a99ed9e12d8d0be9b5b369b649d9b0598f398
malikkurosaki <kurosakiblackangel@gmail.com>
2022-08-03 16:38:36 +0800

    Wed Aug  3 16:38:36 WITA 2022

--- server/index_https_config.js
+++ server/index_https_config.js
@@ -3,9 +3,16 @@ const app = express();
 const fs = require("fs");
 const path = require("path");
 const createServer = require("https").createServer;
+// const httpServer = createServer({
+//         key: fs.readFileSync(path.join(__dirname, "./key.pem")),
+//         cert: fs.readFileSync(path.join(__dirname, "./cert.pem")),
+//     },
+//     app);
+
+
 const httpServer = createServer({
-        key: fs.readFileSync(path.join(__dirname, "./key.pem")),
-        cert: fs.readFileSync(path.join(__dirname, "./cert.pem")),
+    key: fs.readFileSync(path.join(__dirname, " /root/myAwesomeServer.pvk")),
+    cert: fs.readFileSync(path.join(__dirname, "/root/myAwesomeServer.cer")),
     },
     app);
 
